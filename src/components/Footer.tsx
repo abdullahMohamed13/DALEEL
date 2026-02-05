a259abc5260d3757c61fe4243f670de28c4d9592
abdullahMohamed13 <https://github.com/abdullahMohamed13>
2026-02-05 20:41:16 +0200

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,22 @@ import XIcon from '@/assets/icons/Xicon.svg'
 
 export default function Footer() {
     return <footer className="mt-20 px-6 flex justify-between items-center text-muted-foreground gap-10">
-        <div>
-            رفيقك الذكي في المعاملات والرحلات.
-            <br />
-             جميع الحقوق محفوظة
-            2026
+
+        <div className='flex flex-col gap-4'>
+            <div className="flex gap-3 items-center">
+                <div className="flex flex-col text-secondary">
+                    <p className="font-bold text-xl">دليل</p>
+                    <p className="text-sm">Daleel</p>
+                </div>
+                <img src="/logo.png" alt="" />
+            </div>
+
+            <div>
+                رفيقك الذكي في المعاملات والرحلات.
+                <br />
+                جميع الحقوق محفوظة
+                2026
+            </div>
         </div>
         <ul className="flex gap-4 *:cursor-pointer *:hover:underline">
             <li>سياسة الخصوصية</li>
@@ -15,7 +26,7 @@ export default function Footer() {
             <li>الأسئلة الشائعة</li>
             <li>الدعم الفني</li>
         </ul>
-        <div className='flex items-center gap-3 *:border-[#0000000D] *:border *:bg-[#F9FAFB] *:rounded-full *:p-2 '>
+        <div className='flex items-center gap-3 *:border-[#0000000D] *:border *:bg-[#F9FAFB] *:rounded-full *:p-3'>
             <div className=''>
                 <img src={XIcon} alt="" />
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
-
-const savedTheme = localStorage.getItem('theme')
-
-if (savedTheme === 'dark') {
-  document.documentElement.classList.add('dark')
-}
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster />
     <div className="min-h-screen ">
       <App />
     </div>
