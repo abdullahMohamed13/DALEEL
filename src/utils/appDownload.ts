897b0d841e45f63dc70c8ee6f3d7ff9c6fb8880d
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-03-16 00:45:56 +0200

    chore: restructure into monorepo

--- src/utils/appDownload.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from "sonner";
-
-export function appDownload() {
-    toast('التطبيق قيد التطوير', {
-        description: 'سيكون متاحاً للتحميل قريباً، penguins ترقبوا!',
-        icon: '📱',
-    });
-}
